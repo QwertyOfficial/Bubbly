014ddd38abd903d5026d230bea747d7a0030499c
QwertyOfficial <114987790+QwertyOfficial@users.noreply.github.com>
2024-09-29 02:27:39 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ const toggleButton = document.getElementById('toggle-button');
 
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
+    
+    // Toggle card background color in dark mode
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.classList.toggle('dark-mode');
+    });
 });
